bdfb54137bcfbfef6164b791f5b3debf7daa0b11
baseklesser@gmail.com <baseklesser@gmail.com>
2025-07-18 11:55:38 +0500

    Change in charts

--- lib/ui/pages/seller/homepage/page_seller_homepage_home.dart
+++ lib/ui/pages/seller/homepage/page_seller_homepage_home.dart
@@ -2,7 +2,6 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:luma/domain/seller_bloc/seller_account_bloc.dart';
-import 'package:luma/global/params/app_colors.dart';
 import 'package:luma/global/params/app_text_styles.dart';
 import 'package:luma/ui/widgets/widget_seller_item_card_statistic.dart';
 
@@ -28,23 +27,23 @@ class PageSellerHomepageHome extends StatelessWidget {
         ];
 
         final List<FlSpot> spotsB = [
-          FlSpot(1, 14),
-          FlSpot(2, 12),
-          FlSpot(3, 17),
-          FlSpot(4, 21),
-          FlSpot(5, 27),
-          FlSpot(6, 14),
-          FlSpot(7, 11),
+          FlSpot(1, 0),
+          FlSpot(2, 6),
+          FlSpot(3, 11),
+          FlSpot(4, 17),
+          FlSpot(5, 14),
+          FlSpot(6, 21),
+          FlSpot(7, 26),
         ];
 
         final List<FlSpot> spotsC = [
-          FlSpot(1, 21),
+          FlSpot(1, 0),
           FlSpot(2, 27),
           FlSpot(3, 17),
           FlSpot(4, 14),
           FlSpot(5, 14),
           FlSpot(6, 12),
-          FlSpot(7, 11),
+          FlSpot(7, 24),
         ];
 
         return CustomScrollView(
@@ -56,79 +55,19 @@ class PageSellerHomepageHome extends StatelessWidget {
               ),
             ),
 
+            // SliverToBoxAdapter(
+            //   child: LinesGraph(spotsA: spotsA, spotsB: spotsB, spotsC: spotsC),
+            // ),
+
+            // SliverToBoxAdapter(
+            //   child: PieGraph(),
+            // ),
             SliverToBoxAdapter(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Container(
-                  margin: EdgeInsets.all(16),
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  decoration: const BoxDecoration(
-                    color: AppColors.vanillaIce,
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  child: LineChart(
-                    LineChartData(
-                      lineBarsData: [
-                        LineChartBarData(
-                          color: Colors.blueAccent,
-                          isCurved: true,
-                          preventCurveOverShooting: true,
-                          spots: spotsA,
-                        ),
-
-                        LineChartBarData(
-                          color: Colors.purpleAccent,
-                          isCurved: true,
-                          preventCurveOverShooting: true,
-                          spots: spotsB,
-                        ),
-
-                        LineChartBarData(
-                          color: Colors.orangeAccent,
-                          isCurved: true,
-                          preventCurveOverShooting: true,
-                          spots: spotsC,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SliverToBoxAdapter(
-              child: AspectRatio(
-                aspectRatio: 1.0,
-                child: Container(
-                  margin: EdgeInsets.all(16),
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  decoration: const BoxDecoration(
-                    color: AppColors.vanillaIce,
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  // height: 300,
-                  child: PieChart(
-                    PieChartData(
-                      sections: [
-                        PieChartSectionData(
-                          value: 15,
-                          radius: 40,
-                          color: Colors.orangeAccent,
-                        ),
-                        PieChartSectionData(
-                          value: 25,
-                          radius: 50,
-                          color: Colors.purpleAccent,
-                        ),
-                        PieChartSectionData(
-                          value: 60,
-                          radius: 60,
-                          color: Colors.blueAccent,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+              child: Row(
+                children: [
+                  LinesGraph(spotsA: spotsA, spotsB: spotsB, spotsC: spotsC),
+                  PieGraph(),
+                ],
               ),
             ),
 
@@ -218,6 +157,92 @@ class PageSellerHomepageHome extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class LinesGraph extends StatelessWidget {
+  const LinesGraph({
+    super.key,
+    required this.spotsA,
+    required this.spotsB,
+    required this.spotsC,
+  });
+
+  final List<FlSpot> spotsA;
+  final List<FlSpot> spotsB;
+  final List<FlSpot> spotsC;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.width / 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: LineChart(
+          LineChartData(
+            lineBarsData: [
+              LineChartBarData(
+                color: Colors.blueAccent,
+                isCurved: true,
+                preventCurveOverShooting: true,
+                spots: spotsA,
+              ),
+        
+              LineChartBarData(
+                color: Colors.purpleAccent,
+                isCurved: true,
+                preventCurveOverShooting: true,
+                spots: spotsB,
+              ),
+        
+              LineChartBarData(
+                color: Colors.orangeAccent,
+                isCurved: true,
+                preventCurveOverShooting: true,
+                spots: spotsC,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PieGraph extends StatelessWidget {
+  const PieGraph({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.width / 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PieChart(
+          PieChartData(
+            sections: [
+              PieChartSectionData(
+                value: 15,
+                radius: 40,
+                color: Colors.orangeAccent,
+              ),
+              PieChartSectionData(
+                value: 25,
+                radius: 50,
+                color: Colors.purpleAccent,
+              ),
+              PieChartSectionData(
+                value: 60,
+                radius: 60,
+                color: Colors.blueAccent,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
